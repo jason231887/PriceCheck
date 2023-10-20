3582caee3dd28c454e30e83a436ca1d668701026
Jason Zheng <jasonzheng2001@hotmail.com>
2023-10-19 22:29:09 -0500

    removed redundant function, and added comments

--- new.py
+++ new.py
@@ -1,26 +1,5 @@
 from bs4 import BeautifulSoup as bs
 import requests
-import decimal
-
-maxPrice = 10
-def getPrices():
-    url = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=ampharos+prime&_sacat=0&_sop=15'
-
-    # Get the page
-    page = requests.get(url)
-
-    # Parses the html as text
-    soup = bs(page.text,'html5lib')
-    
-    priceList = []
-    prices = soup.find_all("span", {'class' : 's-item__price'})
-    for price in prices:
-        temp = str(price)
-        dollar = temp.partition("$")[2]
-        final = dollar.partition("<")
-        priceList.append(final[0])
-    priceList.pop(0)
-    print(priceList)
 
 def getUrls():
     url = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=ampharos+prime&_sacat=0&_sop=15'
@@ -31,28 +10,34 @@ def getUrls():
     # Parses the html as text
     soup = bs(page.text,'html5lib')
     urlList = []
+    #Find the div where the URL is stored
     divs = soup.find_all("div", {"class": "s-item__info clearfix"})
+    #Find the a tag
     for link in divs:
         links = link.find("a")
+        #If a href is found, then append to list
         if links:
             href = links.get('href')
             urlList.append(href)
+    #Remove the top element as it is not part of the search
     urlList.pop(0)
-    #print(urlList)
     return urlList
 
+#Get the string of the price, and return only the number
 def getNumber(string):
     temp = string.partition(" ")[2]
     if '$' in temp:
         temp = temp.partition('$')[2]
     return(temp)
 
+#Add together the two strings of price and shipping
 def addTogether(str1, str2):
     if str2 == '':
         return round(float(str1), 2)
     final = (float(str1) + float(str2))
     return round(final, 2)
 
+#Get price for each URL in list
 def getPrice(urls):
     priceList = []
     for url in urls:
@@ -66,8 +51,9 @@ def getPrice(urls):
         shippingDiv = soup.find("div", {"class": "ux-labels-values__values-content"})
         shipping = shippingDiv.find("span", {'class' : 'ux-textspans ux-textspans--BOLD'}).text
         shipping = getNumber(shipping)
-
+        #Add the price and shipping together
         fullPrice = addTogether(price, shipping)
+        #Append price to list
         priceList.append(fullPrice)
     print(priceList)
 
