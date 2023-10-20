b7b1272c5afd1a0a374df6fa04769a0a2b2b37d6
Jason Zheng <jasonzheng2001@hotmail.com>
2023-10-19 22:18:17 -0500

    now price and shipping is combined and are rounded to 2 decimal points

--- new.py
+++ new.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup as bs
 import requests
+import decimal
 
+maxPrice = 10
 def getPrices():
     url = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=ampharos+prime&_sacat=0&_sop=15'
 
@@ -39,16 +41,33 @@ def getUrls():
     #print(urlList)
     return urlList
 
+def getNumber(string):
+    temp = string.partition(" ")[2]
+    if '$' in temp:
+        temp = temp.partition('$')[2]
+    return(temp)
+
+def addTogether(str1, str2):
+    if str2 == '':
+        return round(float(str1), 2)
+    final = (float(str1) + float(str2))
+    return round(final, 2)
+
 def getPrice(urls):
     priceList = []
     for url in urls:
         page = requests.get(url)
         soup = bs(page.text,'html5lib')
+        #Get the price of the item
         priceDiv = soup.find("div", {"class": "x-price-primary"})
         price = priceDiv.find("span", {'class' : 'ux-textspans'}).text
+        price = getNumber(price)
+        #Get the price of shipping
         shippingDiv = soup.find("div", {"class": "ux-labels-values__values-content"})
         shipping = shippingDiv.find("span", {'class' : 'ux-textspans ux-textspans--BOLD'}).text
-        fullPrice = price + " + " + shipping
+        shipping = getNumber(shipping)
+
+        fullPrice = addTogether(price, shipping)
         priceList.append(fullPrice)
     print(priceList)
 
