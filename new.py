59bf790b6e4f4621ab27a7b8dbf187982c530645
Jason Zheng <jasonzheng2001@hotmail.com>
2023-10-23 15:57:28 -0500

    added comments

--- new.py
+++ new.py
@@ -1,18 +1,25 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
+#Final list to be output
 fullList = []
-urlList = []
+#List of searches to be parsed
+searchList = []
+#List of max prices for each item
 maxList = []
+#The amount of searches in input.txt to be searched
 length = 0
 
 def getLinks():
     global length
+    #Open input file of search urls
     input = open('input.txt', 'r')
+
+    #Go through each line and grab the search link and max price
     for each in input:
         url = each.partition(',')[0]
         max = each.partition(',')[2]
-        urlList.append(url)
+        searchList.append(url)
         maxList.append(max)
         length = length + 1
     input.close()
@@ -20,11 +27,14 @@ def getLinks():
 def getListings(url):
     # Get the page
     page = requests.get(url)
+
     # Parses the html as text
     soup = bs(page.text,'html5lib')
     urlList = []
+
     #Find the div where the URL is stored
     divs = soup.find_all("div", {"class": "s-item__info clearfix"})
+    
     #Find the a tag
     for link in divs:
         links = link.find("a")
@@ -32,6 +42,7 @@ def getListings(url):
         if links:
             href = links.get('href')
             urlList.append(href)
+
     #Remove the top element as it is not part of the search
     urlList.pop(0)
     return urlList
@@ -56,37 +67,51 @@ def getPrice(urls, max):
     global fullList
     page = requests.get(urls)
     soup = bs(page.text,'html5lib')
+
     #Get the price of the item
     priceDiv = soup.find("div", {"class": "x-price-primary"})
     price = priceDiv.find("span", {'class' : 'ux-textspans'}).text
     price = getNumber(price)
+
     #Get the price of shipping
     shippingDiv = soup.find("div", {"class": "ux-labels-values__values-content"})
     shipping = shippingDiv.find("span", {'class' : 'ux-textspans ux-textspans--BOLD'}).text
     shipping = getNumber(shipping)
-    #Add the price and shipping together
+
+    #If shipping isn't a float, then only take price into account
     if type(shipping) != 'float':
         fullPrice = price
     else:
         fullPrice = addTogether(price, shipping)
     fullPrice = float(fullPrice)
+
+    #If price is more than the max, then move onto next item
     if fullPrice > max:
         return False
+    
     #Append price to list
-    fullList.append([urls, fullPrice])
+    fullList.append([urls])
     return True
+
 #Main
+
+#Get search links from input.txt
 getLinks()
+
+#For each search URL, go through each item until max price is hit
 for each in range(length):
-    url = getListings(urlList[each])
+    url = getListings(searchList[each])
     for link in url:
         if (getPrice(link, maxList[each])):
             continue
+        #Max price has been hit, and move onto next item
         else: 
             break
 
+#Open output file and write the list of URL's to it
 output = open("output.txt",'w')
 for each in fullList:
-    output.write(str(each))
+    output.write('%s\n' %each)
 output.close()
+
 print(fullList)
